a1e647438dd0d82becd752b1e2a07868b8bdcbb8
teevik <teemu.vikoren@gmail.com>
2026-01-10 01:22:39 +0100

    ci: Switch from Nix to cargo-xwin for Windows cross-compilation

--- flake.nix
+++ flake.nix
@@ -3,11 +3,6 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     blueprint.url = "github:numtide/blueprint";
     blueprint.inputs.nixpkgs.follows = "nixpkgs";
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    crane.url = "github:ipetkov/crane";
   };
 
   outputs =
